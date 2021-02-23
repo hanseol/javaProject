e8e571d9ed0bbf0f033665ac834477b2f0d4f39d
admin <admin@YD01-19>
2021-02-23 13:47:39 +0900

    4th git

--- javaProject/src/controls/WhileExample2.java
+++ javaProject/src/controls/WhileExample2.java
@@ -2,24 +2,21 @@ package controls;
 
 public class WhileExample2 {
 	public static void main(String[] args) {
-		
-		for(int i=2; i<=3; i++) {
-		showResult(i);
-		}
+		showResult(5);
 	}
 	
 	public static void showResult(int num) {
 		int i=1;
-		while(i<=9) {
-		System.out.println(num + "*" + i + "=" + num*i);
-		i++;
-		}
-		
-//		while(true) {
-//			if(i<=9) {
-//				System.out.println(num + "*" + i + "=" + num*i);
-//			}
-//			i++;
+//		while(i<=9) {
+//		System.out.println(num + "*" + i + "=" + num*i);
+//		i++;
 //		}
+		while(true) {
+			System.out.println(num + "*" + i + "=" + num*i);
+			i++;
+			if(i>=10) {
+				break;
+			}
+		}
 	}
 }
