34ef02e941ead2663d3699394dc9eb38762cad39
gkstj <gkstj@DESKTOP-OKKIC4N>
2021-03-26 23:53:48 +0900

    score

--- yedamEmpl/src/myapp/score/RootController.java
+++ yedamEmpl/src/myapp/score/RootController.java
@@ -12,6 +12,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -21,80 +23,143 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import myapp.viewPackage.Phone;
-
 
 public class RootController implements Initializable {
 
-	@FXML TableView<Student> tableView;
-	@FXML Button addBtn;
-	@FXML TextField korean, math, english;
-	
-	private Stage primaryStage;
-	
-	public void setprimaryStage(Stage primaryStage) {
-		this.primaryStage = primaryStage;
+	@FXML
+	TableView<Student> tableView;
+	@FXML
+	Button addBtn, barchartBtn;
+	@FXML
+	TextField korean, math, english;
+
+	private Stage insertStage;
+	private Stage chartStage;
+
+	public void setinsertStage(Stage insertStage) {
+		this.insertStage = insertStage;
 	}
-	
-	
+
+	public void setchartStage(Stage chartStage) {
+		this.chartStage = chartStage;
+	}
+
 	ObservableList<Student> list = FXCollections.observableArrayList();
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
-	        TableColumn<Student, String> tcName = (TableColumn<Student,String>)tableView.getColumns().get(0);
-	        tcName.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
-			tcName.setStyle("-fx-alignment: CENTER;");
-			TableColumn<Student, Integer> tcKorean = (TableColumn<Student,Integer>)tableView.getColumns().get(1);
-			tcKorean.setCellValueFactory(new PropertyValueFactory<Student,Integer>("korean"));
-			tcKorean.setStyle("-fx-alignment: CENTER;");
-			TableColumn<Student, Integer> tcMath = (TableColumn<Student,Integer>)tableView.getColumns().get(2);
-			tcMath.setCellValueFactory(new PropertyValueFactory<Student,Integer>("math"));
-			tcMath.setStyle("-fx-alignment: CENTER;");
-			TableColumn<Student, Integer> tcEnglish = (TableColumn<Student,Integer>)tableView.getColumns().get(3);
-			tcEnglish.setCellValueFactory(new PropertyValueFactory<Student,Integer>("english"));
-			tcEnglish.setStyle("-fx-alignment: CENTER;");
+		TableColumn<Student, String> tcName = (TableColumn<Student, String>) tableView.getColumns().get(0);
+		tcName.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+		tcName.setStyle("-fx-alignment: CENTER;");
+		TableColumn<Student, Integer> tcKorean = (TableColumn<Student, Integer>) tableView.getColumns().get(1);
+		tcKorean.setCellValueFactory(new PropertyValueFactory<Student, Integer>("korean"));
+		tcKorean.setStyle("-fx-alignment: CENTER;");
+		TableColumn<Student, Integer> tcMath = (TableColumn<Student, Integer>) tableView.getColumns().get(2);
+		tcMath.setCellValueFactory(new PropertyValueFactory<Student, Integer>("math"));
+		tcMath.setStyle("-fx-alignment: CENTER;");
+		TableColumn<Student, Integer> tcEnglish = (TableColumn<Student, Integer>) tableView.getColumns().get(3);
+		tcEnglish.setCellValueFactory(new PropertyValueFactory<Student, Integer>("english"));
+		tcEnglish.setStyle("-fx-alignment: CENTER;");
+
+	}
+
+	public void showBtnAction(ActionEvent event) {
+
+		Stage stage = new Stage(StageStyle.DECORATED);
+		stage.initModality(Modality.WINDOW_MODAL);
+		stage.initOwner(chartStage);
+
+		try {
+			BorderPane bp = FXMLLoader.load(getClass().getResource("Barchart.fxml"));
+			Scene scene = new Scene(bp);
+			stage.setScene(scene);
+			stage.show();
+
+			BarChart barChart = (BarChart) bp.lookup("#barChart");
+
+			XYChart.Series<String, Integer> series1 = new XYChart.Series<String, Integer>();
+			ObservableList<XYChart.Data<String, Integer>> data1 = FXCollections.observableArrayList();
+			for (int i = 0; i < list.size(); i++) {
+				data1.add(new XYChart.Data<String, Integer>(list.get(i).getName(), list.get(i).getKorean()));
+			}
+			series1.setData(data1);
+			series1.setName("국어");
+
+			XYChart.Series<String, Integer> series2 = new XYChart.Series<String, Integer>();
+			ObservableList<XYChart.Data<String, Integer>> data2 = FXCollections.observableArrayList();
+			for (int i = 0; i < list.size(); i++) {
+				data2.add(new XYChart.Data<String, Integer>(list.get(i).getName(), list.get(i).getMath()));
+			}
+			series2.setData(data2);
+			series2.setName("수학");
+
+			XYChart.Series<String, Integer> series3 = new XYChart.Series<String, Integer>();
+			ObservableList<XYChart.Data<String, Integer>> data3 = FXCollections.observableArrayList();
+			for (int i = 0; i < list.size(); i++) {
+				data3.add(new XYChart.Data<String, Integer>(list.get(i).getName(), list.get(i).getEnglish()));
+			}
+			series3.setData(data3);
+			series3.setName("영어");
+
+			barChart.getData().add(series1);
+			barChart.getData().add(series2);
+			barChart.getData().add(series3);
 			
+			Button closeBtn = (Button) bp.lookup("#closeBtn");
+			closeBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					stage.close();
+				}
+				
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void addBtnAction(ActionEvent e) {
 		Stage stage = new Stage(StageStyle.DECORATED);
 		stage.initModality(Modality.WINDOW_MODAL);
-		stage.initOwner(primaryStage);
-		
+		stage.initOwner(insertStage);
+
 		try {
 			BorderPane bp = FXMLLoader.load(getClass().getResource("Form.fxml"));
 			Scene scene = new Scene(bp);
 			stage.setScene(scene);
 			stage.show();
 
-			Button btnSave =(Button) bp.lookup("#btnSave");
+			Button btnSave = (Button) bp.lookup("#btnSave");
 			btnSave.setOnAction(new EventHandler<ActionEvent>() {
-				
+
 				@Override
 				public void handle(ActionEvent arg0) {
-					
+
 					TextField txtName = (TextField) bp.lookup("#name");
 					TextField txtKorean = (TextField) bp.lookup("#korean");
 					TextField txtMath = (TextField) bp.lookup("#math");
 					TextField txtEnglish = (TextField) bp.lookup("#english");
-					
+
 //					Student student = new Student(txtName.getText(),
 //								Integer.parseInt(txtKorean.getText()),
 //								Integer.parseInt(txtMath.getText()),
 //								Integer.parseInt(txtEnglish.getText()));
-					
+
 					Student student = new Student();
 					student.setName(txtName.getText());
 					student.setKorean(Integer.parseInt(txtKorean.getText()));
 					student.setMath(Integer.parseInt(txtMath.getText()));
 					student.setEnglish(Integer.parseInt(txtEnglish.getText()));
-					
+
 					list.add(student);
 					tableView.setItems(list);
 				}
 			});
-			
+
 			Button btnCancle = (Button) bp.lookup("#btnCancel");
 			btnCancle.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
@@ -102,7 +167,7 @@ public class RootController implements Initializable {
 					stage.close();
 				}
 			});
-	
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
